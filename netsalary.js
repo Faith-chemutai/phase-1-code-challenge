6e8a4c9298a8e863354b8f5a40b919c20c2fa287
Faith <faithchemutai2330@gmail.com>
2022-12-08 15:50:19 +0300

    completed week-1-code challenge

--- netsalary.js
+++ netsalary.js
@@ -1,78 +1,85 @@
+//
+let salary;
+let benefits;
+let NHIF;
+let NSSf;
+let PAYE;
 let preBasicSalary =prompt("Input basic salary");
-let benefit = prompt("input benefits")
-let basicSalary = preBasicSalary - benefits;
-//PAYE
-if (basicSalary<= 24000){
-    paye = basicSalary /10;
-}else if (basicSalary >= 24001 && basicSalary <=3233){
-    paye =basicSalary/4;
-}else if ((basicSalary >32,333)){
-    paye =(basicSalary *3)/10;
+console.log(preBasicSalary)
+function salCalc(salary,benefits){
+    let gross = salary + benefits;
+    let NSSF = gross * 0.06;
+    //let netCalculator = gross;
+    //PAYE
+    if(gross <= 24000){
+        PAYE = (gross*0.01)
+    }
+    else if(gross >= 32333){
+        PAYE = (gross*0.3)
+    }
+    else if(gross >= 24001){
+        PAYE = (gross*0.25)
+    }
+    console.log('gross salary:', gross)
+    console.log('PAYE deductions:',PAYE)
+    console.log('NSSF deduction:',NSSF)
+    //NHIF Deduction
 
+    if(gross <= 5999 ){
+        deduction = 150
+    }
+    else if (gross >= 6000 && gross <= 7999){
+        deduction = 300
+    }
+    else if (gross >= 8000 && gross <= 11999){
+        deduction = 400
+    }
+    else if (gross >= 12000 && gross <= 14999){
+        deduction = 500
+    }
+    else if (gross >= 15000 && gross <= 19999){
+        deduction = 600
+    }
+    else if (gross >= 20000 && gross <= 24999){
+        deduction = 750
+    }
+    else if (gross >= 25000 && gross <= 29999){
+        deduction = 850
+    }
+    else if (gross >= 30000 && gross <= 34999){
+        deduction = 900
+    }
+    else if (gross >= 35000 && gross <= 39999){
+        deduction = 950
+    }
+    else if (gross >= 40000 && gross <= 44999){
+        deduction = 1000
+    }
+    else if (gross >= 45000 && gross <= 49999){
+         deduction = 1100
+    }
+    else if (gross >= 50000 && gross <= 59999){
+        deduction = 1200
+    }
+    else if (gross >= 60000 && gross <= 69999){
+       deduction = 1300
+    }
+    else if (gross >= 70000 && gross <= 79999){
+        deduction = 1400
+    }
+    else if (gross >= 80000 && gross <= 89999){
+        deduction = 1500
+    }
+    else if (gross >= 90000 && gross <= 99999){
+        deduction = 1600
+    }
+    else { 
+        deduction =  1700 
+    console.log(`NHIF deduction: ${deduction}`);
+    console.log('Taxes: PAYE + NSSF + NHIF:',(NSSF + PAYE + NHIF));
+    let netSalary = 'NetSalary:'+(gross-(deduction));
+    return netSalary;
+    }
 }
-console.log(`paye:$paye}`);
+console.log(salCalc(100000,5000));
 
-//Nhif deductions
-if (basicSalary<= 5999){
-    deductionN=150;
-}else if  (basicSalary >=6000 && basicSalary<= 7999){
-    deductionN=300;
-
-}else if  (basicSalary >=8000 && basicSalary<= 11999){
-    deduction=400;
-
-}else if  (basicSalary >=12000 && basicSalary<= 14999){
-    deduction=500;
-}else if  (basicSalary >=15000 && basicSalary<= 19999){
-    deduction =600;
-}else if  (basicSalary >=20000 && basicSalary<= 24999){
-    deduction=750;
-}else if  (basicSalary >=25000 && basicSalary<= 29999){
-    deduction=850;
-}else if  (basicSalary >=30000 && basicSalary<= 34999){
-    deduction=900;
-
-}else if  (basicSalary >=35000 && basicSalary<= 39999){
-    deduction=950;
-}else if  (basicSalary >=40000 && basicSalary<= 44999){
-    deduction =1000;
-
-}else if  (basicSalary >=45000 && basicSalary<= 49999){
-    deduction =1100;
-}else if  (basicSalary >=50000 && basicSalary<= 59999){
-    deduction =1200;
-
-}else if  (basicSalary >=60000 && basicSalary<= 69999){
-    deduction =1300;
-
-}else if  (basicSalary >=70000 && basicSalary<= 79999){
-    deduction =1400;
-}else if  (basicSalary >=80000 && basicSalary<= 89999){
-    deduction =1500;
-}else if  (basicSalary >=90000 && basicSalary<= 99999){
-    deduction=1600;
-}else if  (basicSalary >=100000){
-    deduction =1700;
-}
-console.log(`NHIF:${deduction}`);
-// NSSF deductions
-let nssf=(basicSalary*6)/100;
-console.log(`NSSF:${nssf}`);
-let netSalary =basicSalary - paye-deduction;
-console.log(`Net Salary:${netSalary}`);
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-     
-    
--- speeddetector.js
+++ speeddetector.js
@@ -1,19 +1,17 @@
-
-const SpeedDetector =require ('prompt-user');
-const prompt =SpeedDetector();
+const speedDetector = require('prompt-sync');
+const prompt = speedDetector();
+// calculate speed
 function speedMonitor(){
-    let speed =prompt('speed:');
-    if (speed <=70){
-     console.log('ok');
+    let speed = prompt('speed:');//use prompt for input
+    if(speed <= 70){
+        console.log('Ok');//
     }
-      else if (speed >70 && speed <=130){
-        let demeritPoint =(speed -70)/5;
-        console.log (`points:$demeritpoint`);
-   
-      }else {
-        console.log("license suspended")
+    else if(speed > 70 && speed <= 130){
+        let demeritPoint =  (speed-70)/5;
+        console.log(`points: ${demeritPoint}`);//calculate demerit points
+    }
+    else{
+         console.log("License suspended")
     }
 }
 speedMonitor();
-
-   
