a5207f92148c59d3530a1a9dc0aa2a10d74b1cb8
Faith <faithchemutai2330@gmail.com>
2022-12-14 15:14:52 +0300

    completed week 1 code challenge

--- grade.js
+++ grade.js
@@ -1,34 +1,26 @@
-//A > 79
- //B - 60 to 79
-  //C -  59 to 40
-  // D - 40 to 49
-  // E - less 40
+const grade = require('prompt-sync');
+const prompt = grade();
+function generator(){
+    let grade = prompt('Enter your mark:');
+// calculate using function
+    if(grade <= 100 && grade >= 79){
+        console.log('Scored A');
+    }
+    else if(grade >= 60 && grade <= 79){
+        console.log('Scored B');
+    }
+    else if(grade >= 49 && grade <= 59){
+        console.log('Scored C');
+    }
+    else if(grade >= 40 && grade <= 49){
+        console.log('Scored D');
+    }
+    else if(grade < 40){
+        console.log('Scored E');
+    }
+    else{
+        console.log('enter valid marks');
+    }
+}
 
-  const grade = require('prompt-sync');
-  const prompt = grade();
-  function generator(){
-      let grade = prompt('Enter your mark:');
-  // calculate using function
-      if(grade <= 100 && grade >= 79){
-          console.log('Scored A');
-      }
-      else if(grade >= 60 && grade <= 79){
-          console.log('Scored B');
-      }
-      else if(grade >= 49 && grade <= 59){
-          console.log('Scored C');
-      }
-      else if(grade >= 40 && grade <= 49){
-          console.log('Scored D');
-      }
-      else if(grade < 40){
-          console.log('Scored E');
-      }
-      else{
-          console.log('enter valid marks');
-      }
-  }
-  
-  generator();
-  
-  
+generator();
